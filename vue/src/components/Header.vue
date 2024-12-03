1dd990da2b4c1af16506816a9f7680782cd95e2f
priya <lakshmipriya.skm@gmail.com>
2024-12-03 11:52:42 -0500

    Update

---
+++ vue/src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="head-banner">
+        <h1>Welcome to Roccos Pizza</h1>
+        
+          
+        </div>
+    </template>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
+    <header>
+      
+    </header>
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
   </div>
