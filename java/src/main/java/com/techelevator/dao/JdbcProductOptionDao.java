de4f8cb99e8db0a89c075b25e9a0ca97e8d31711
Zachary Harris <zharris94@gmail.com>
2024-12-06 16:38:59 -0500

    fixed @component missing on jdbc dao

--- java/src/main/java/com/techelevator/dao/JdbcProductOptionDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcProductOptionDao.java
@@ -6,11 +6,13 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class JdbcProductOptionDao implements ProductOptionDao{
     private final JdbcTemplate jdbcTemplate;
 
