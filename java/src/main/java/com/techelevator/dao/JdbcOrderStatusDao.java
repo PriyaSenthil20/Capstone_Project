b281a0cd72ff2e1f083c15caaa08043e6b3bb5e4
Zachary Harris <zharris94@gmail.com>
2024-12-12 11:13:43 -0500

    revision to Order objects in Java

--- java/src/main/java/com/techelevator/dao/JdbcOrderStatusDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcOrderStatusDao.java
@@ -2,13 +2,11 @@ package com.techelevator.dao;
 
 import com.techelevator.exception.DaoException;
 import com.techelevator.model.OrderStatus;
-import com.techelevator.model.Product;
 import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,6 +33,21 @@ public class JdbcOrderStatusDao implements OrderStatusDao{
             throw new DaoException("Unable to connect to server or database", e);
         }
         return orderStatuses;
+    }
+
+    @Override
+    public OrderStatus getOrdersStatusById(int orderStatusId) {
+        OrderStatus orderStatus = new OrderStatus();
+        String sql  = "SELECT status_id, status_type FROM order_status WHERE status_id = ?";
+        try {
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql,orderStatusId);
+            if (results.next()) {
+                orderStatus = mapRowToOrderStatus(results);
+            }
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+        }
+        return orderStatus;
     }
 
     private OrderStatus mapRowToOrderStatus(SqlRowSet rs) {
