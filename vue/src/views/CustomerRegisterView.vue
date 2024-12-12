92065a70555246ec824abc3d6063d8ecb7b98e90
Sri Durga Ambica Devi Varre <ambica.varre@gmail.com>
2024-12-12 15:49:32 -0500

    add pic

--- vue/src/views/CustomerRegisterView.vue
+++ vue/src/views/CustomerRegisterView.vue
@@ -239,7 +239,7 @@ export default {
 
 <style scoped>
 #register {
-  background-image: url('../assets/register5.png'); 
+  background-image: url('../assets/register9.jpg'); 
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
@@ -249,7 +249,7 @@ export default {
   align-items: center;
 }
 #customerRegister{
-  background-image: url('../assets/register5.png'); 
+  background-image: url('../assets/register9.jpg'); 
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
