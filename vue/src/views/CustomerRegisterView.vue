2ce79fa86d81a0009533cb5b0366ad0bab4f225a
Patrick Sylvester <devpsylvester@gmail.com>
2024-12-12 17:22:15 -0500

    inventory functionality update

--- vue/src/views/CustomerRegisterView.vue
+++ vue/src/views/CustomerRegisterView.vue
@@ -212,7 +212,7 @@ export default {
           .then((response) => {
             if (response.status == 201) {
               this.$store.commit("SET_CUSTOMER", response.data);
-              
+              this.$router.push('/');
             }
           })
           .catch((error) => {
